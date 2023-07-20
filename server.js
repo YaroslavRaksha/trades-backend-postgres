a59d137b2ef33053e2bf4c6908b06aff13f6bdc6
remido <rakshayaroslav37@gmail.com>
2023-07-20 18:00:02 +0300

    port removed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 const port = process.env.PORT || 3001;
-const host =  process.env.HOST || '192.168.1.131';
 
-app.listen(port, host, () => {
-    console.log(`server running on ${host}:${port}`)
+app.listen(port, () => {
+    console.log(`server running on ${port}`)
 })
