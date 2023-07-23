06b368e58a194c3c60bcdfa59a1a2eae3a9e76d8
remido <rakshayaroslav37@gmail.com>
2023-07-23 18:42:38 +0300

    commit

--- app/helpers/executeQuery.js
+++ app/helpers/executeQuery.js
@@ -1,10 +1,11 @@
 const pg = require('pg');
 const { Pool } = pg;
 
-
 // postgres://default:T4LqQDYka2Hf@ep-jolly-resonance-114869-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb
+
+// postgresql://postgres:aIGzkEdYmVU15KrSsifq@containers-us-west-37.railway.app:7903/railway
 const pool = new Pool({
-    connectionString: process.env.POSTGRE_URL + "?sslmode=require",
+    connectionString: "postgresql://postgres:aIGzkEdYmVU15KrSsifq@containers-us-west-37.railway.app:7903/railway",
 })
 
 const executeQuery = (query, params = []) => {
