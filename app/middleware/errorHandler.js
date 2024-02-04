82867a5d25cb7bf4a370518ae8f552a0f8a3644f
Yaroslav <71005329+YaroslavRaksha@users.noreply.github.com>
2024-02-04 09:12:00 +0200

    Update errorHandler.js

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -1,12 +1,11 @@
 
 const errorHandler = (error, req, res, next) => {
-
-    console.log(error);
+    
     const generatedError = error?.error?.status ? error?.error : {
         timestamp: Date.now(),
         type: 'rest',
         status: 500,
-        message: 'Internal server error at error handler',
+        message: `Internal server error at error handler ${error}`,
     };
 
     return res.status(generatedError.status).json({ error: generatedError });
