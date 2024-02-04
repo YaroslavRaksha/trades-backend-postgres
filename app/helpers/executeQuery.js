8c8d7b40056f6ca8ab47ee1f69fe73028b588379
Yaroslav <71005329+YaroslavRaksha@users.noreply.github.com>
2024-02-04 09:16:55 +0200

    Update executeQuery.js

--- app/helpers/executeQuery.js
+++ app/helpers/executeQuery.js
@@ -1,11 +1,6 @@
 const pg = require('pg');
 const { Pool } = pg;
 
-// postgres://default:T4LqQDYka2Hf@ep-jolly-resonance-114869-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb
-
-// postgresql://postgres:aIGzkEdYmVU15KrSsifq@containers-us-west-37.railway.app:7903/railway
-// postgresql://postgres:11cGDGefBcAeC52eB22fF32dafBcBGGA@roundhouse.proxy.rlwy.net:34220/railway
-
 const pool = new Pool({
     connectionString: process.env.POSTGRE_URL,
 })
